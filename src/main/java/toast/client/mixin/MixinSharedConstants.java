20c27b78ecedcd6d80199d791750f92e4e56026c
Qther <dtaclanlegion@gmail.com>
2020-05-04 11:46:46 +0800

    Change Illegal Character Bypass to use @Inject

--- src/main/java/toast/client/mixin/MixinSharedConstants.java
+++ src/main/java/toast/client/mixin/MixinSharedConstants.java
@@ -3,6 +3,9 @@ package toast.client.mixin;
 import net.minecraft.SharedConstants;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SharedConstants.class)
 public class MixinSharedConstants {
@@ -10,8 +13,8 @@ public class MixinSharedConstants {
      * @author Morgz
      * Makes you able to "chat" § etc (normally "illegal" characters)
      */
-    @Overwrite
-    public static boolean isValidChar(char chr) {
-        return true;
+    @Inject(method = "isValidChar", at = @At("HEAD"), cancellable = true)
+    private static void isValidChar(CallbackInfoReturnable info) {
+        info.setReturnValue(true);
     }
 }
