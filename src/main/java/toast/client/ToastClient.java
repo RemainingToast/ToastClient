a757801ec51d690e4a3da4ac2e3a0f5f2867907c
RemainingToast <remainingtoast@gmail.com>
2020-05-25 02:48:44 +1000

    Start on Discord RPC

--- src/main/java/toast/client/ToastClient.java
+++ src/main/java/toast/client/ToastClient.java
@@ -3,9 +3,9 @@ package toast.client;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Formatting;
 import toast.client.commands.CommandHandler;
-import toast.client.event.EventManager;
 import toast.client.modules.ModuleManager;
 import toast.client.utils.FileManager;
 import toast.client.utils.LoginUtil;
@@ -16,6 +16,9 @@ import java.util.List;
 
 @Environment(EnvType.CLIENT)
 public class ToastClient implements ModInitializer {
+
+    public static String APP_ID = "669916916290420736";
+
     public static String version = "b1.0";
     public static String cleanPrefix = "ToastClient";
     public static String chatPrefix = Formatting.DARK_GRAY+"["+Formatting.LIGHT_PURPLE+"ToastClient"+Formatting.DARK_GRAY+"]";
@@ -26,12 +29,14 @@ public class ToastClient implements ModInitializer {
 
     @Override
     public void onInitialize() {
+
         System.out.println(cleanPrefix+" Initialized");
         System.out.println("Special thanks to all contributors of this project "+devs);
         FileManager.initFileManager();
         ModuleManager.initModules();
         CommandHandler.initCommands();
         TPSCalculator.calculatorInstance = new TPSCalculator();
+        if(MinecraftClient.getInstance().getSession().getUsername().contains("Player")){devMode = true;}
         if(devMode) {
             boolean login = LoginUtil.loginCracked("ToastDeveloper"); // avoid generating lots of .dat files in singleplayer worlds
             if (login) {
@@ -40,6 +45,8 @@ public class ToastClient implements ModInitializer {
                 System.out.println("Failed login ;C");
             }
         }
+        System.out.println("Starting RPC");
+        //RPC
+        DiscordPresence.start();
     }
-
 }
