421b97a0126bdbdcdfb94ec13e1903b22ee4f6a2
willem <wnuke9@gmail.com>
2020-06-08 19:14:50 +0200

    convert autorespawn to kotlin

--- src/main/java/toast/client/modules/combat/AutoRespawn.kt
+++ src/main/java/toast/client/modules/combat/AutoRespawn.kt
@@ -1,38 +1,40 @@
-package toast.client.modules.combat;
+package toast.client.modules.combat
 
-import net.minecraft.client.gui.screen.DeathScreen;
-import toast.client.event.EventImpl;
-import toast.client.event.events.player.EventUpdate;
-import toast.client.modules.Module;
-import toast.client.utils.TimerUtil;
+import net.minecraft.client.gui.screen.DeathScreen
+import toast.client.event.EventImpl
+import toast.client.event.events.player.EventUpdate
+import toast.client.modules.Module
+import toast.client.utils.TimerUtil
 
-public class AutoRespawn extends Module {
-
-    private final TimerUtil timer = new TimerUtil();
-
-    public AutoRespawn() {
-        super("AutoRespawn", "Automatically presses the respawn button for you.", Category.COMBAT, -1);
-        this.settings.addSlider("Speed", 1, 2, 20);
+class AutoRespawn : Module(
+    "AutoRespawn",
+    "Automatically presses the respawn button for you.",
+    Category.COMBAT,
+    -1
+) {
+    private val timer = TimerUtil()
+    override fun onEnable() {
+        timer.reset()
     }
 
-    public void onEnable() {
-        timer.reset();
-    }
-
-    public void onDisable() {
-        timer.reset();
+    override fun onDisable() {
+        timer.reset()
     }
 
     @EventImpl
-    public void onEvent(EventUpdate event) {
+    fun onEvent(event: EventUpdate?) {
         if (mc.currentScreen != null) {
-            if (this.timer.isDelayComplete((long) (this.getDouble("Speed") * 1000L))) {
-                this.timer.setLastMS();
-                if (mc.currentScreen instanceof DeathScreen) {
-                    mc.player.requestRespawn();
-                    mc.openScreen(null);
+            if (timer.isDelayComplete((getDouble("Speed") * 1000L).toLong())) {
+                timer.setLastMS()
+                if (mc.currentScreen is DeathScreen) {
+                    mc.player!!.requestRespawn()
+                    mc.openScreen(null)
                 }
             }
         }
+    }
+
+    init {
+        settings.addSlider("Speed", 1.0, 2.0, 20.0)
     }
 }
