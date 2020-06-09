fe55cc57c23b05d49fcd2560d7d3d0326d78aca0
willem <wnuke9@gmail.com>
2020-06-09 15:44:35 +0200

    convert command manager to kotlin

--- src/main/java/toast/client/commands/CommandHandler.kt
+++ src/main/java/toast/client/commands/CommandHandler.kt
@@ -1,76 +1,76 @@
-package toast.client.commands;
+package toast.client.commands
 
-import net.minecraft.client.MinecraftClient;
-import toast.client.ToastClient;
-import toast.client.commands.cmds.*;
-import toast.client.utils.Logger;
+import net.minecraft.client.MinecraftClient
+import toast.client.ToastClient
+import toast.client.commands.cmds.*
+import toast.client.commands.cmds.Set
+import toast.client.utils.Logger
+import java.util.*
+import java.util.concurrent.CopyOnWriteArrayList
 
-import java.util.Objects;
-import java.util.concurrent.CopyOnWriteArrayList;
-
-public class CommandHandler {
-    public static CopyOnWriteArrayList<Command> commands = new CopyOnWriteArrayList<>();
-
-    public void executeCmd(String name, String[] args) {
-        boolean notfound = true;
-        for (Command command : commands) {
-            for (String alias : command.aliases) {
-                if (alias.equalsIgnoreCase(name)) {
+class CommandHandler {
+    fun executeCmd(name: String, args: Array<String>) {
+        var notfound = true
+        for (command in commands) {
+            for (alias in command.aliases) {
+                if (alias.equals(name, ignoreCase = true)) {
                     try {
                         if (isDevCancel(command)) {
-                            notfound = true;
-                            continue;
+                            notfound = true
+                            continue
                         }
-                        notfound = false;
-                        command.run(args);
-                    } catch (Exception err) {
-                        err.printStackTrace();
-                        Logger.message("Sorry but something went wrong", Logger.ERR, true);
+                        notfound = false
+                        command.run(args)
+                    } catch (err: Exception) {
+                        err.printStackTrace()
+                        Logger.message("Sorry but something went wrong", Logger.ERR, true)
                     }
                 }
             }
         }
         if (notfound) {
-            Logger.message("Cannot find command " + ToastClient.cmdPrefix + name, Logger.ERR, true);
+            Logger.message("Cannot find command " + ToastClient.cmdPrefix + name, Logger.ERR, true)
         }
     }
 
-    public boolean isDevCancel(Command c) {
-        return c.isDev() && !ToastClient.devs.contains(Objects.requireNonNull(MinecraftClient.getInstance().player).getDisplayName().asFormattedString());
+    fun isDevCancel(c: Command): Boolean {
+        return c.isDev && !ToastClient.devs.contains(Objects.requireNonNull(MinecraftClient.getInstance().player)!!.displayName.asFormattedString())
     }
 
-    public CopyOnWriteArrayList<Command> getCommands() {
-        return commands;
-    }
+    val commands: CopyOnWriteArrayList<Command> = CopyOnWriteArrayList()
 
-    public Command getCommand(String cmd) {
-        for (Command command : commands) {
-            for (String alias : command.aliases) {
-                if (alias.equalsIgnoreCase(cmd)) {
-                    return command;
+    fun getCommand(cmd: String?): Command? {
+        for (command in commands) {
+            for (alias in command.aliases) {
+                if (alias.equals(cmd, ignoreCase = true)) {
+                    return command
                 }
             }
         }
-        return null;
+        return null
     }
 
-    public void initCommands() {
-        commands.clear();
+    fun initCommands() {
+        commands.clear()
         // alphabetical order please
-        commands.add(new Bind());
-        commands.add(new ClearChat());
-        commands.add(new FOV());
-        commands.add(new GuiReset());
-        commands.add(new Help());
-        commands.add(new ListModules());
-        commands.add(new Macro());
-        commands.add(new MOTD());
-        commands.add(new Panic());
-        commands.add(new Prefix());
-        commands.add(new Reload());
-        commands.add(new Save());
-        commands.add(new Suffix());
-        commands.add(new Set());
-        commands.add(new Toggle());
+        commands.add(Bind())
+        commands.add(ClearChat())
+        commands.add(FOV())
+        commands.add(GuiReset())
+        commands.add(Help())
+        commands.add(ListModules())
+        commands.add(Macro())
+        commands.add(MOTD())
+        commands.add(Panic())
+        commands.add(Prefix())
+        commands.add(Reload())
+        commands.add(Save())
+        commands.add(Suffix())
+        commands.add(Set())
+        commands.add(Toggle())
+    }
+
+    companion object {
+        var commands = CopyOnWriteArrayList<Command>()
     }
 }
