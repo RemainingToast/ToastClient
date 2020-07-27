62c039f10a85a91fa432c1b8f032602ea9f68fb3
Qther <dtaclanlegion@gmail.com>
2020-07-27 21:40:48 +0800

    MessageUtil.java > MessageUtil.kt

--- src/main/java/dev/toastmc/client/mixin/client/MixinClientConnection.java
+++ src/main/java/dev/toastmc/client/mixin/client/MixinClientConnection.java
@@ -9,7 +9,6 @@ import net.minecraft.network.listener.PacketListener;
 import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.IInjectionPointContext;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
