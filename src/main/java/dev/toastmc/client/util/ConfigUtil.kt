922272751ec519a45c6079e0820f50ce0b2bddd0
Qther <dtaclanlegion@gmail.com>
2020-09-09 22:05:24 +0800

    ConfigUtils: untested get and set

--- src/main/java/dev/toastmc/client/util/ConfigUtil.kt
+++ src/main/java/dev/toastmc/client/util/ConfigUtil.kt
@@ -5,6 +5,7 @@ import dev.toastmc.client.ToastClient.Companion.MODULE_MANAGER
 import io.github.fablabsmc.fablabs.api.fiber.v1.annotation.AnnotatedSettings
 import io.github.fablabsmc.fablabs.api.fiber.v1.builder.ConfigTreeBuilder
 import io.github.fablabsmc.fablabs.api.fiber.v1.exception.ValueDeserializationException
+import io.github.fablabsmc.fablabs.api.fiber.v1.schema.type.derived.ConfigTypes
 import io.github.fablabsmc.fablabs.api.fiber.v1.serialization.FiberSerialization
 import io.github.fablabsmc.fablabs.api.fiber.v1.serialization.JanksonValueSerializer
 import io.github.fablabsmc.fablabs.api.fiber.v1.tree.ConfigBranch
@@ -12,6 +13,8 @@ import net.minecraft.client.MinecraftClient
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
+import java.math.BigDecimal
+import java.math.BigInteger
 
 class ConfigUtil {
     private var module: File? = null
@@ -58,5 +61,61 @@ class ConfigUtil {
         } catch (ignored: ValueDeserializationException){
             println("Config failed to load. \n\nStackTrace:\n${ignored.stackTrace}")
         }
+    }
+
+    fun ConfigTreeBuilder.getInt(settingName: String): Int? {
+        return this.lookupLeaf(settingName, ConfigTypes.INTEGER.serializedType)?.value?.toInt()
+    }
+
+    fun ConfigTreeBuilder.setInt(settingName: String, value: Int) {
+        this.lookupLeaf(settingName, ConfigTypes.INTEGER.serializedType)?.value = BigDecimal(value)
+    }
+
+    fun ConfigTreeBuilder.setLong(settingName: String, value: Long) {
+        this.lookupLeaf(settingName, ConfigTypes.LONG.serializedType)?.value = BigDecimal(value)
+    }
+
+    fun ConfigTreeBuilder.getBigInteger(settingName: String): BigInteger? {
+        return this.lookupLeaf(settingName, ConfigTypes.UNBOUNDED_INTEGER.serializedType)?.value?.toBigIntegerExact()
+    }
+
+    fun ConfigTreeBuilder.setBigInteger(settingName: String, value: BigInteger) {
+        this.lookupLeaf(settingName, ConfigTypes.UNBOUNDED_INTEGER.serializedType)?.value = value.toBigDecimal()
+    }
+
+    fun ConfigTreeBuilder.getDouble(settingName: String): Double? {
+        return this.lookupLeaf(settingName, ConfigTypes.DOUBLE.serializedType)?.value?.toDouble()
+    }
+
+    fun ConfigTreeBuilder.setDouble(settingName: String, value: Double) {
+        this.lookupLeaf(settingName, ConfigTypes.DOUBLE.serializedType)?.value = BigDecimal(value)
+    }
+
+    fun ConfigTreeBuilder.getFloat(settingName: String): Float? {
+        return this.lookupLeaf(settingName, ConfigTypes.FLOAT.serializedType)?.value?.toFloat()
+    }
+
+    fun ConfigTreeBuilder.setFloat(settingName: String, value: Float) {
+        this.lookupLeaf(settingName, ConfigTypes.FLOAT.serializedType)?.value = BigDecimal(value.toDouble())
+    }
+
+    fun ConfigTreeBuilder.getBigDecimal(settingName: String): BigDecimal? {
+        return this.lookupLeaf(settingName, ConfigTypes.UNBOUNDED_DECIMAL.serializedType)?.value
+    }
+
+    fun ConfigTreeBuilder.setBigDecimal(settingName: String, value: BigDecimal) {
+        this.lookupLeaf(settingName, ConfigTypes.UNBOUNDED_DECIMAL.serializedType)?.value = value
+    }
+
+    fun ConfigTreeBuilder.getLong(settingName: String): Long? {
+        return this.lookupLeaf(settingName, ConfigTypes.LONG.serializedType)?.value?.longValueExact()
+    }
+
+    fun ConfigTreeBuilder.getBoolean(settingName: String): Boolean? {
+        return this.lookupLeaf(settingName, ConfigTypes.BOOLEAN.serializedType)?.value
+    }
+
+    fun ConfigTreeBuilder.setBoolean(settingName: String, value: Boolean) {
+        this.lookupLeaf(settingName, ConfigTypes.BOOLEAN.serializedType)?.value = value
     }
 }
