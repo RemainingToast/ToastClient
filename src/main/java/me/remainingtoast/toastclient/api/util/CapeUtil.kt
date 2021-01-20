0429b45c0d171bd7a456921bdf8951cc3f5fee4f
RemainingToast <remainingtoast@gmail.com>
2021-01-20 20:11:10 +1000

    Added Cape Auth

--- src/main/java/me/remainingtoast/toastclient/api/util/CapeUtil.kt
+++ src/main/java/me/remainingtoast/toastclient/api/util/CapeUtil.kt
@@ -1,11 +1,27 @@
 package me.remainingtoast.toastclient.api.util
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
 import java.util.*
 
 object CapeUtil {
 
-    fun hasCape(uuid: UUID): Boolean {
-        return true;
+    var capeTxt = "https://raw.githubusercontent.com/RemainingToast/toastclient/dev/cape-uuids.txt"
+    var uuids: MutableList<UUID> = ArrayList()
+
+    init {
+        try {
+            val url = URL(capeTxt)
+            val br = BufferedReader(InputStreamReader(url.openStream()))
+            val line: String = ""
+            while (line == br.readLine()){
+                uuids.add(UUID.fromString(line))
+            }
+        } catch (e: Exception) {}
     }
 
+    fun hasCape(uuid: UUID): Boolean {
+        return uuids.contains(uuid);
+    }
 }
