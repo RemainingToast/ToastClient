98beb881143a1306976c2e2d5bcfe0c44eb88e07
RemainingToast <remainingtoast@gmail.com>
2021-06-01 23:02:55 +1000

    start on hud editor

--- src/main/kotlin/dev/toastmc/toastclient/impl/gui/hud/HUDEditorScreen.kt
+++ src/main/kotlin/dev/toastmc/toastclient/impl/gui/hud/HUDEditorScreen.kt
@@ -1,10 +1,14 @@
 package dev.toastmc.toastclient.impl.gui.hud
 
+import dev.toastmc.toastclient.api.util.TwoDRenderUtil
 import dev.toastmc.toastclient.api.util.lit
 import net.minecraft.client.gui.screen.Screen
+import net.minecraft.client.util.math.MatrixStack
 
 class HUDEditorScreen : Screen(lit("HUDEditor")) {
 
-
+    override fun render(matrices: MatrixStack, mouseX: Int, mouseY: Int, delta: Float) {
+        TwoDRenderUtil.drawRect(matrices, 1, 1, 1, 1, -1)
+    }
 
 }
