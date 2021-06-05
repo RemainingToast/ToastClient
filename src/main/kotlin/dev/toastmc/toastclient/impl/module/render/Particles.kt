dc8f38ec97b5f15c2ce1d08a9cdf69227ead6dee
Qther <dtaclanlegion@gmail.com>
2021-06-05 09:36:52 +0800

    Death particles

--- src/main/kotlin/dev/toastmc/toastclient/impl/module/render/Particles.kt
+++ src/main/kotlin/dev/toastmc/toastclient/impl/module/render/Particles.kt
@@ -1,11 +1,15 @@
 package dev.toastmc.toastclient.impl.module.render
 
+import dev.toastmc.toastclient.api.events.EntityEvent
 import dev.toastmc.toastclient.api.events.PlayerAttackEntityEvent
 import dev.toastmc.toastclient.api.managers.module.Module
+import dev.toastmc.toastclient.api.setting.Setting
 import dev.toastmc.toastclient.api.util.RandomUtil
 import net.minecraft.entity.decoration.EndCrystalEntity
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.particle.*
 import net.minecraft.util.Identifier
+import net.minecraft.util.math.Vec3d
 import net.minecraft.util.registry.Registry
 import org.quantumclient.energy.Subscribe
 
@@ -15,9 +19,9 @@ object Particles : Module("Particles", Category.RENDER) {
 
     var crystalToggle = bool("Toggle", true)
     var crystalParticle = mode("Particle", "smoke", particleNames)
-    var crystalRad = number("Radius", 1.0, 1.0, 10.0)
+    var crystalRad = number("Radius", 1.0, 0.0, 10.0)
     var crystalCount = number("Count", 100, 1, 1000)
-    var crystalSpeed = number("Speed", 1.0, 0.1, 5.0)
+    var crystalSpeed = number("Speed", 1.0, 0.0, 5.0)
     var crystals = group("Crystals",
         crystalToggle,
         crystalParticle,
@@ -26,25 +30,65 @@ object Particles : Module("Particles", Category.RENDER) {
         crystalSpeed
     )
 
+    var deathToggle = bool("Toggle", true)
+    var deathKills = bool("KillsOnly", true)
+    var deathPlayers = bool("PlayersOnly", true)
+    var deathParticle = mode("Particle", "damage_indicator", particleNames)
+    var deathRad = number("Radius", 0.0, 0.0, 10.0)
+    var deathCount = number("Count", 1, 1, 1000)
+    var deathSpeed = number("Speed", 0.0, 0.0, 5.0)
+    var deaths = group("Deaths",
+        deathToggle,
+        deathKills,
+        deathPlayers,
+        deathParticle,
+        deathRad,
+        deathCount,
+        deathSpeed
+    )
+
     @Subscribe
     fun on(event: PlayerAttackEntityEvent) {
-        if (mc.player == null) return
-        if (crystalToggle.value && event.entity is EndCrystalEntity) {
-            val pos = event.entity.pos
-            RandomUtil.nextCloud(crystalRad.value, crystalCount.value.toInt(), pos).forEach { vec ->
-                val motionVector = vec.subtract(pos).normalize().multiply(crystalSpeed.value)
-                val particle = particleTypes.entries.filter { idToName(it.key.value) == crystalParticle.value }.getOrNull(0)
+        if (mc.player == null || !crystalToggle.value ) return
 
-                mc.world!!.addParticle(
-                    if (particle == null) ParticleTypes.SMOKE else particle.value as ParticleEffect,
-                    vec.x,
-                    vec.y,
-                    vec.z,
-                    motionVector.x,
-                    motionVector.y,
-                    motionVector.z
-                )
-            }
+        val ent = event.entity
+        if (ent is EndCrystalEntity) {
+            val pos = ent.pos.add(0.0, ent.eyeY / 2, 0.0)
+            createParticleCloud(getParticle(crystalParticle, ParticleTypes.SMOKE), RandomUtil.nextCloud(crystalRad.value, crystalCount.value.toInt(), pos), pos, crystalSpeed.value)
+        }
+    }
+
+    @Subscribe
+    fun on(event: EntityEvent.EntityDeath) {
+        if (mc.player == null || !deathToggle.value) return
+
+        val ent = event.entity
+        if ((deathPlayers.value && ent !is PlayerEntity)
+            || (ent is PlayerEntity && ent == mc.player)
+            || (deathKills.value && event.source.attacker != mc.player)) return
+
+        val pos = ent.pos.add(0.0, ent.eyeY / 2, 0.0)
+        createParticleCloud(getParticle(deathParticle, ParticleTypes.DAMAGE_INDICATOR), RandomUtil.nextCloud(deathRad.value, deathCount.value.toInt(), pos), pos, deathSpeed.value)
+    }
+
+    fun getParticle(mode: Setting.Mode, default: ParticleEffect): ParticleEffect {
+        val particle = particleTypes.entries.filter { idToName(it.key.value) == mode.value }.getOrNull(0)
+        return if (particle == null) default else particle.value as ParticleEffect
+    }
+
+    fun createParticleCloud(particleEffect: ParticleEffect, cloud: List<Vec3d>, center: Vec3d, speed: Double) {
+        cloud.forEach { vec ->
+            val motionVector = vec.subtract(center).normalize().multiply(speed)
+
+            mc.world!!.addParticle(
+                particleEffect,
+                vec.x,
+                vec.y,
+                vec.z,
+                motionVector.x,
+                motionVector.y,
+                motionVector.z
+            )
         }
     }
 
@@ -52,7 +96,7 @@ object Particles : Module("Particles", Category.RENDER) {
         return (if (id.namespace == "minecraft") "" else "${id.namespace}:") + id.path.toString()
     }
 
-    fun recoverPath(name: String): String {
-        return (if (name.contains(":")) "" else "minecraft:") + name
-    }
+//    fun recoverPath(name: String): String {
+//        return (if (name.contains(":")) "" else "minecraft:") + name
+//    }
 }
