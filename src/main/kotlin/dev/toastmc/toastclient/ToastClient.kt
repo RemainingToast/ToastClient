8b8e14160b476992d1640926e5fcc53b1eed8ef2
SkiddyToast <45642596+SkiddyToast@users.noreply.github.com>
2021-06-13 04:08:10 -0400

    uploading for testing purposes

--- src/main/kotlin/dev/toastmc/toastclient/ToastClient.kt
+++ src/main/kotlin/dev/toastmc/toastclient/ToastClient.kt
@@ -5,6 +5,7 @@ import dev.toastmc.toastclient.api.managers.command.CommandManager
 import dev.toastmc.toastclient.api.managers.module.ModuleManager
 import dev.toastmc.toastclient.api.util.font.FontAccessor
 import dev.toastmc.toastclient.api.util.font.StringRenderer
+import dev.toastmc.toastclient.impl.module.render.Capes
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents
 import org.quantumclient.energy.EventBus
@@ -28,6 +29,8 @@ open class ToastClient : ModInitializer, IToastClient {
             CommandManager.init()
 
             ConfigUtil.init()
+
+            Capes.init()
 
             logger.info("Finished loading $nameVersion (${System.currentTimeMillis() - start}ms)")
         }
--- src/main/kotlin/dev/toastmc/toastclient/impl/module/render/Capes.kt
+++ src/main/kotlin/dev/toastmc/toastclient/impl/module/render/Capes.kt
@@ -1,15 +1,54 @@
 package dev.toastmc.toastclient.impl.module.render
 
+import com.google.gson.reflect.TypeToken
+import dev.toastmc.toastclient.IToastClient
 import dev.toastmc.toastclient.api.managers.module.Module
+import dev.toastmc.toastclient.api.util.network.ConnectionUtil
+import net.minecraft.util.Identifier
+import java.lang.reflect.Type
+import java.util.*
 
-object Capes : Module("Capes", Category.RENDER) {
+object Capes : Module("Capes", Category.RENDER), IToastClient {
 
-    private val capeTypes = arrayListOf(
-        "OLD_MOJANG",
-        "MINECON2013",
-        "MINECON2016"
+    private val users = hashMapOf<UUID, String>()
+
+    private val capes = arrayListOf(
+        Identifier("toastclient", "capes/old_mojang.png"),
+        Identifier("toastclient", "capes/toast.png"),
+        Identifier("toastclient", "capes/minecon_2013.png"),
     )
 
-    var capeType = mode("Type", "OLD_MOJANG", capeTypes)
+    private val capeTypeStrings = arrayListOf(
+        "NONE",
+        "CONTRIBUTOR",
+        "PLUS"
+    )
+
+    var capeType = mode("Type", "NONE", capeTypeStrings)
+
+    fun getIdentifierFromMode(): Identifier {
+        when(capeType.value) {
+            capeTypeStrings[0] -> return capes[0]
+            capeTypeStrings[1] -> return capes[1]
+            capeTypeStrings[2] -> return capes[2]
+            else -> {
+                return capes[0]
+            }
+        }
+    }
+
+    fun init() {
+        val type: Type = object : TypeToken<Map<UUID, String>>(){}.getType()
+        if(ConnectionUtil.getJsonFromUrl("http://localhost/capes.json") != "") {
+            val cachedUsers: Map<UUID, String> =
+                gson.fromJson<Map<UUID, String>>(ConnectionUtil.getJsonFromUrl("http://localhost/capes.json"), type)
+            for (entry in cachedUsers.entries) {
+                users.put(entry.key, entry.value)
+            }
+            logger.info(users.entries.toString())
+        } else {
+            logger.error("Failed to get capes.")
+        }
+    }
 
 }
